ca1480f50a207d618cee36170c343ea07410c11e
Kanik Goyal <63117613+7999999166@users.noreply.github.com>
2024-04-05 18:51:46 +0530

    dvcdv

--- config/middlewares.js
+++ config/middlewares.js
@@ -22,7 +22,7 @@ module.exports = [
     config: {
       enabled: true,
       headers: '*',
-      origin: ['http://localhost:5173','http://localhost:1337','https://accessible-approval-ce70603f49.strapiapp.com']
+      origin: ['http://localhost:1337','https://accessible-approval-ce70603f49.strapiapp.com','https://strapiback-production.up.railway.app']
     }
   },
   'strapi::query',
